487d7dec1649d13a9b6b7808854bdd9581ca9566
Unknown <tkwan4@ucsc.edu>
2017-12-07 16:56:11 -0800

    added some visualization

--- sketch.js
+++ sketch.js
@@ -2,7 +2,16 @@ import * as MidiConvert from 'midiconvert';
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
+//var Q = require('q');
+var notes;
+var max1 = -1;
+var max2;
+var min1 = 10000;
+var min2;
+var measure;
+var loaded = false;
 var partsData;
+var tracksNum;
 const sketch = (p5) => {
   const width = p5.windowWidth;
   const height = p5.windowHeight;
@@ -10,14 +19,57 @@ const sketch = (p5) => {
   window.p5 = p5;
 
   p5.setup = () => {
-    let canvas = p5.createCanvas(height, width);
-    canvas.parent('sketch');
-    p5.background(0);
-    p5.fill(255, 255, 255);
-    p5.text("word", 10, 90);
-    // dropzone = select('#dropzone');
-    canvas.drop(gotFile);
-    //p5.rect(230, 425, 150, 150);
+      let canvas = p5.createCanvas(width, height);
+      canvas.parent('sketch');
+      p5.background(0);
+      p5.fill(255, 255, 255);
+      p5.text("word", 10, 90);
+      p5.frameRate(10);
+      // dropzone = select('#dropzone');
+      canvas.drop(gotFile);
+      //p5.rect(230, 425, 150, 150);
+    }
+    //p5.draw = () => {
+    //if (!loaded) {
+    //put loading animation
+    //} else {
+
+  //}
+  var i = 0;
+  p5.draw = () => {
+    // Re add you button here
+    if (loaded) {
+      for (let x = 0; x < notes.length; x++) {
+        //console.log(notes[x].name);
+        if (notes[x].midi > max1) {
+          max1 = notes[x].midi;
+        }
+        if (notes[x].midi < min1) {
+          min1 = notes[x].midi;
+        }
+      }
+      //for (let x = 0; x < notes.length; x++) {
+      if (i < notes.length) {
+        console.log(notes[i].name);
+        p5.rectMode(p5.CORNERS)
+          // notes[i].time is where the note start
+          // notes[i].duration is how long the not plays for
+          // notes[i].midi is the pitch
+          //partsData.tracks[1].duration is how long the song is
+        p5.fill((notes[i].time / partsData.tracks[1].duration) * 255, 0, 0);
+        p5.rect((notes[i].time / partsData.tracks[1].duration) * width,
+          height - ((notes[i].midi / max1) * height), ((notes[i].duration +
+            notes[i].time) / partsData.tracks[1].duration) * width,
+          height - ((notes[i].midi / max1) * height) + 5);
+        i++;
+      } else {
+        i = 0;
+        p5.background(0);
+      }
+
+
+      //}
+    }
   }
 
   function gotFile(file) {
@@ -36,21 +88,28 @@ const sketch = (p5) => {
 
       partsData = MidiConvert.parse(e.target.result);
       //console.log(JSON.stringify(partsData, undefined, 2));
-    }
-    reader.onloadend = function(e) {
-      console.log(partsData);
-      var test = JSON.stringify(partsData.tracks, "tracks", 2);
-      //partsData.tracks.length
       var print = "nothing";
       for (let i = 0; i < partsData.tracks.length; i++) {
         //print = "nothing";
         console.log(partsData.tracks[i].channelNumber);
         if (partsData.tracks[i].channelNumber > -1) {
           print = JSON.stringify(partsData.tracks[i], 2);
+          tracksNum = i;
           console.log("hi");
         }
-        p5.background(0);
-        p5.text(print, 10, 90);
+        notes = partsData.tracks[1].notes;
+      }
+      reader.onloadend = function(e) {
+        console.log(partsData);
+        //partsData.tracks.length
+        //find the range of notes
+        //finding seconds per measure
+
+        loaded = true;
+        //console.log("max:" + max1);
+        //console.log("min: " + min1);
+        //p5.background(0);
+        //p5.text(print, 10, 90);
 
       }
 
