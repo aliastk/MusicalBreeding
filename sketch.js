2ee2155d28f37091ae027e3be30ada82b274c098
aliastk <tkwan4@ucsc.edu>
2017-12-12 14:17:22 -0800

    allow multiple files to be read

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
 import Tone from "tone";
 //var Q = require('q');
-var notes, index;
+var index;
 var max1 = -1;
-var max2;
 var min1 = 10000;
-var min2;
-var measure;
+var measure, PPS;
 var loaded = false;
+var songs = [];
+var PPQN;
 var partsData;
 var tracksNum;
 const sketch = (p5) => {
@@ -25,7 +25,7 @@ const sketch = (p5) => {
       p5.background(0);
       p5.fill(255, 255, 255);
       p5.text("word", 10, 90);
-      //p5.frameRate(10);
+      p5.frameRate(10);
       // dropzone = select('#dropzone');
       canvas.drop(gotFile);
 
@@ -52,8 +52,9 @@ const sketch = (p5) => {
     }
     index = 0;
     //p5.background(0);
-    count_measure = measure;
-    play();
+    count_measure = songs[0].measure;
+    play(songs[0]);
+    makeDisplay(songs[0], width / 4, height / 4);
   }
 
   p5.draw = () => {
@@ -62,179 +63,135 @@ const sketch = (p5) => {
     p5.fill(red, green, blue);
     p5.rect(50, 250, 50, 50);
     if (loaded && green == 255) {
-      /*for (let x = 0; x < notes.length; x++) {
-        //console.log(notes[x].name);
-        if (notes[x].midi > max1) {
-          max1 = notes[x].midi;
-        }
-        if (notes[x].midi < min1) {
-          min1 = notes[x].midi;
-        }
-      }
-      //for (let x = 0; x < notes.length; x++) {
-      if (i < notes.length) {
-        console.log(notes[i].name);
-        p5.rectMode(p5.CORNERS)
-          // notes[trackNum].time is where the note start
-          // notes[i].duration is how long the not plays for
-          // notes[i].midi is the pitch
-          //partsData.tracks[1].duration is how long the song is
-        p5.fill((notes[i].time / partsData.tracks[tracksNum].duration) *
-          255, 0, 0);
-        p5.rect((notes[i].time / partsData.tracks[tracksNum].duration) *
-          width,
-          height - ((notes[i].midi / max1) * height), ((notes[i].duration +
-            notes[i].time) / partsData.tracks[tracksNum].duration) *
-          width,
-          height - ((notes[i].midi / max1) * height) + 5);
-        i++;
-      } else {
-        i = 0;
-        p5.background(0);
-      }*/
-      console.log(notes[index].name);
-      //p5.background(0);
-      p5.rectMode(p5.CORNERS)
-        // notes[trackNum].time is where the note start
-        // notes[i].duration is how long the not plays for
-        // notes[i].midi is the pitch
-        //partsData.tracks[1].duration is how long the song is
-      p5.fill((notes[index].time / partsData.tracks[tracksNum].duration) *
-        255, 0, (1 - (notes[index].time / partsData.tracks[tracksNum].duration)) *
-        255);
-      p5.rect((notes[index].time / partsData.tracks[tracksNum].duration) *
-        width,
-        height - ((notes[index].midi / max1) * height), ((notes[index].duration +
-          notes[index].time) / partsData.tracks[tracksNum].duration) *
-        width,
-        height - ((notes[index].midi / max1) * height) + 5);
-      if ((notes[index].duration + notes[index].time) >= count_measure) {
-        console.log(count_measure);
-        var wide = (count_measure / partsData.tracks[tracksNum].duration) *
-          width;
-        p5.fill(255)
-        p5.ellipse(wide, max1, 3, min1);
-        count_measure += measure;
-      }
-    } else {
-      //p5.background(0);
+      //console.log(index.name);
+      //display(index);
     }
   }
 
+
   function gotFile(file) {
     p5.createP(file.name + " " + file.size);
-    /*p5.fill(255, 255, 255);
-    p5.text("word", 10, 90);
-    //let img = p5.createImg(file.data);
-    p5.rect(0, 0, 200, 200);*/
-    //p5.image(img, 0, 0, 200, 200);
-    console.log(file);
+    //console.log(file);
     var stuff = dataURLtoBlob(file.data);
-    console.log(stuff);
-    //MidiConvert.parse(file);
+    //console.log(stuff);
     var reader = new FileReader();
     reader.onload = function(e) {
 
       partsData = MidiConvert.parse(e.target.result);
       console.log(JSON.stringify(partsData, undefined, 2));
-      var print = "nothing";
       for (let i = 0; i < partsData.tracks.length; i++) {
-        //print = "nothing";
         console.log(partsData.tracks[i].channelNumber);
         if (partsData.tracks[i].channelNumber > -1) {
           print = JSON.stringify(partsData.tracks[i], 2);
           tracksNum = i;
-          //console.log("hi");
         }
       }
       //find the measure
-      var PPQN = partsData.header.PPQ;
+      PPQN = partsData.header.PPQ;
       var BPM = partsData.header.bpm;
       var dd = partsData.header.timeSignature[1];
       var nn = partsData.header.timeSignature[0];
       var Qpm = (4 / dd) * nn;
       var Nm = Qpm * PPQN;
-      // find the pulse per second
-      var PPS = (PPQN * BPM) / 60;
+      PPS = (PPQN * BPM) / 60;
       measure = Qpm * PPQN * (1 / PPS);
-      console.log(PPQN);
-      console.log(BPM);
-      console.log(dd);
-      console.log(PPS);
-      console.log(measure);
     }
 
     reader.onloadend = function(e) {
       console.log(partsData);
-      notes = partsData.tracks[1].notes;
+      var notes = partsData.tracks[1].notes;
       //partsData.tracks.length
       //find the range of notes
       //finding seconds per measure
+      notes.max = max1;
+      notes.min = min1;
+      notes.duration = Math.round(partsData.tracks[tracksNum].duration *
+        PPS);
       for (let x = 0; x < notes.length; x++) {
         //console.log(notes[x].name);
-        if (notes[x].midi > max1) {
-          max1 = notes[x].midi;
+        if (notes[x].midi > notes.max) {
+          notes.max = notes[x].midi;
         }
-        if (notes[x].midi < min1) {
-          min1 = notes[x].midi;
+        if (notes[x].midi < notes.min) {
+          notes.min = notes[x].midi;
         }
+        notes[x].type = Math.round((notes[x].duration * PPS));
+        notes[x].offset = Math.round((notes[x].time * PPS));
+        if (x > 0) {
+          notes[x].distance = notes[x].offset - notes[x - 1].offset;
+        } else {
+          notes[x].distance = 0;
+        }
+        notes[x].time = notes[x].offset * (1 / PPS);
+        notes[x].duration = notes[x].type / PPS;
       }
-
-      var rectProp = new Array();
-      //for (let x = 0; x < notes.length; x++) {
-      /*var color = (notes[x].time / partsData.tracks[tracksNum].duration);
-      var start = (notes[x].time / partsData.tracks[tracksNum].duration) *
-        width;
-      var pitch = height - (((notes[x].midi) / max1) * height);
-      var length = (notes[x].duration / partsData.tracks[tracksNum].duration) *
-        width;
-      rectProp.push(color, start, pitch, length);*/
-
-      //}
-      //p5.rectMode(p5.CORNERS);
+      notes.measure = measure;
+      notes.pps = PPS;
+      notes.bpm = partsData.header.bpm;
       index = 0;
       loaded = true;
-
-      //use the events to play the synth
-
-      //console.log("max:" + max1);
-      //console.log("min: " + min1);
-      //p5.background(0);
-      //p5.text(print, 10, 90);
-
+      songs.push(notes);
+      console.log(songs[songs.length - 1]);
     }
 
     reader.readAsBinaryString(stuff)
   }
 
 
-  function play() {
-    Tone.Transport.bpm.value = partsData.header.bpm
+  function play(music) {
+    Tone.Transport.bpm.value = music.bpm
+    var count = 0;
     var synth = new Tone.Synth().toMaster();
     var midiPart = new Tone.Part(function(time, note) {
 
         //use the events to play the synth
         synth.triggerAttackRelease(note.name, note.duration,
-          time,
-          note.velocity);
+          time, note.velocity);
         Tone
           .Draw.schedule(function() {
-            //this callback is invoked from a requestAnimationFrame
+            //this callback is in voked from a requestAnimationFrame
             //and will be invoked close to AudioContext time
-            console.log(note);
-            index++;
-
-            //p5.background(0);
-
+            //console.log(note);
+            //index = note;
           }, time);
+        Tone.Transport.loop = false;
       },
-      notes).start();
+      music).start();
     Tone.Transport.start();
   }
-  //p5.save(partsData, 'my.json');
 
+  function display(curr) {
+    console.log(curr.name);
+    var notes = songs[0];
+    p5.rectMode(p5.CORNERS)
+    p5.fill(curr.time * 255, 0, (1 - (curr.time / notes.duration)) * 255);
+    /*p5.rect((curr.time / notes.duration) * width,
+      height - ((curr.midi / notes.max) * height), ((curr.duration + curr.time) /
+        notes.duration) * width,
+      height - ((curr.midi / notes.max) * height) + 5);
+    if ((curr.duration + curr.time) >= count_measure) {
+      //console.log(count_measure);
+      var wide = (count_measure / curr.duration) * width;
+      p5.fill(255)
+      p5.ellipse(wide, notes.max, 3, notes.min);
+      count_measure += notes.measure;
+    }*/
+  }
 
-
+  function makeDisplay(song, wide, high) {
+    console.log(song);
+    song.forEach(function(curr) {
+      console.log(curr.name);
+      p5.rectMode(p5.CORNERS)
+      p5.fill((curr.offset / song.duration) * 255, 0, (1 - (curr.offset /
+        song.duration)) * 255);
+      p5.rect((curr.offset / song.duration) * wide,
+        high - ((curr.midi / song.max) * high), ((curr.type +
+            curr.offset) /
+          song.duration) * wide,
+        high - ((curr.midi / song.max) * high) + 5);
+    });
+  }
   //A quick fix to get around p5.js method of handling files
   function dataURLtoBlob(dataurl) {
     var arr = dataurl.split(','),
